94551adce6fd6e67af0b80ab18138fb73060c9f7
mmonk3 <lawluker@gmail.com>
2022-09-30 17:24:26 +0500

    sorting

--- Test.py
+++ Test.py
@@ -1,37 +1,27 @@
-import time
-import appium.webdriver.extensions.keyboard
-from appium.webdriver.common.touch_action import TouchAction
-from selenium.common import TimeoutException
-from selenium.webdriver.common.by import By
-
-from Base import Actions, Locators
+from Base import Locators
+from Main import MainPage
 
 
 def test_scroll(browser):
-    launcher = Actions(browser)
-    # launcher.swipe_element('down', Locators.MAIN_MENU_LIST)
-    launcher.find_element(Locators.RUSSIA_SELECTOR).click()
-    launcher.find_element(Locators.SKIP_BUTTON).click()
-    launcher.find_element(Locators.MAIN_BANNER).click()
-    text1 = launcher.find_element(Locators.TOOLBAR_TITLE).text
-    launcher.find_element(Locators.TAB_BAR_MAIN).click()
-    launcher.swipe_element('left', Locators.MAIN_BANNER)
-    launcher.find_element(Locators.MAIN_BANNER).click()
-    text2 = launcher.find_element(Locators.TOOLBAR_TITLE).text
+    main_page = MainPage(browser)
+    main_page.click_element(Locators.RUSSIA_SELECTOR)
+    main_page.click_element(Locators.SKIP_BUTTON)
+    main_page.click_element(Locators.MAIN_BANNER)
+    text1 = main_page.get_element_text(Locators.TOOLBAR_TITLE)
+    main_page.click_element(Locators.TAB_BAR_MAIN)
+    main_page.swipe_element('left', Locators.MAIN_BANNER)
+    main_page.click_element(Locators.MAIN_BANNER)
+    text2 = main_page.get_element_text(Locators.TOOLBAR_TITLE)
+    main_page.click_element(Locators.TAB_BAR_MAIN)
     assert text1 != text2
-    # launcher.find_element(Locators.TAB_BAR_CATALOGUE).click()
-    # search = launcher.find_element(Locators.SEARCH)
-    # search.click()
-    # search_a = launcher.find_element(Locators.SEARCH_A)
-    # search_a.send_keys('T-shirt')
-    # launcher.driver.execute_script('mobile: performEditorAction', {'action': 'search'})
-    # launcher.find_element(Locators.FILTER).click()
-    # launcher.find_element(Locators.CATEGORY).click()
-    # while launcher.is_present(Locators.POLO) is False:
-    #     try:
-    #         launcher.find_element(Locators.POLO).click()
-    #     except:
-    #         launcher.swipe_element('up', Locators.FILTER_AREA)
-    #launcher.swipe_element('down', Locators.FILTER_AREA)
-    time.sleep(3)
-    #launcher.swipe('left', Locators.MAIN_MENU_BANNER)
+
+
+def test_sorting(browser):
+    main_page = MainPage(browser)
+    main_page.click_element(Locators.RUSSIA_SELECTOR)
+    main_page.click_element(Locators.SKIP_BUTTON)
+    main_page.click_element(Locators.TAB_BAR_CATALOGUE)
+    #main_page.click_element(Locators.SEARCH)
+    main_page.enter_text(Locators.SEARCH_A, 'T-shirt')
+    main_page.keyboard_search()
+
